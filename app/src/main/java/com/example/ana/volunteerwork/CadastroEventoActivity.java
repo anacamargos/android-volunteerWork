c22af1d13df02d5e4160f2b64261c2ab5d2aef45
Gabriel Luciano <gabrielluciano26@gmail.com>
2017-12-05 18:14:54 -0200

    Alterações necessárias.

--- app/src/main/java/com/example/ana/volunteerwork/CadastroEventoActivity.java
+++ app/src/main/java/com/example/ana/volunteerwork/CadastroEventoActivity.java
@@ -39,7 +39,7 @@ public class CadastroEventoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_participarevento);
+        setContentView(R.layout.activity_cadastro_evento);
 
 
         // INITIALIZE FIREBASE DB
